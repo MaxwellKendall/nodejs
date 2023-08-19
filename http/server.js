71c15941ff3a5a0a3f85a1fe082ea87755b67dec
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2023-08-19 13:04:26 -0400

    http: add switch statement

--- http/server.js
+++ http/server.js
@@ -1,13 +1,22 @@
-const http = require('http');
+const http = require('http'); // native module
 
 const PORT = 3000;
 
 const handler = (req, res) => {
-    console.log({ path: req.path, method: req.method })
+    console.log({ path: req.route, method: req.method })
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({
-      data: 'Hello World!',
-    }));  
+      data: 'Hello Worldz!',
+    }));
+
+    switch(req.method) {
+        case 'GET': {
+            // handle all get requests
+        }
+        case 'POST': {
+            // handle all POST request
+        }
+    }
 }
 
 // https://nodejs.org/api/http.html#httpcreateserveroptions-requestlistener
@@ -16,7 +25,6 @@ const server = http.createServer(handler);
 server.listen(PORT, (err) => {
     if (err) {
         console.error('Oh no! There was an error!', err);
-
     } else {
         console.info(`Server is listening on port ${PORT}`);
     }
